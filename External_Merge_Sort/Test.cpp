29932c7ba9dcbd17b165a9c63e8ff0385a6751be
sfeng56 <87674230+sfeng56@users.noreply.github.com>
2024-04-09 14:14:29 -0500

    Add files via upload

--- External_Merge_Sort/Test.cpp
+++ External_Merge_Sort/Test.cpp
@@ -1,10 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <unistd.h>
+#include <cstdlib>
 #include "Iterator.h"
 #include "Scan.h"
 #include "Filter.h"
 #include "Sort.h"
+#include "verification.h"
+
+bool generateInput(int numberOfRecords, int recordSize);
 
 int main (int argc, char * argv [])
-{
+{	
+	//--------------------------Generate input file input_table, create output files output_table and trace0.txt
+	#pragma region filePrep
+	int numberOfRecords;
+	int recordSize;
+
+	if (atoi(argv[4]) > 2000) {
+		recordSize = 2000;
+	} else{
+		recordSize = atoi(argv[4]);
+	}
+
+	numberOfRecords = atoi(argv[2]);
+	
+	if (!generateInput(numberOfRecords,recordSize)){
+		std::cout << "Error: having issue generating input. Please investigate.";
+		return 0;
+	};
+    #pragma endregion filePrep
+
+
 	TRACE (true);
 
 	Plan * const plan = new ScanPlan (7);
@@ -12,9 +40,57 @@ int main (int argc, char * argv [])
 
 	Iterator * const it = plan->init ();
 	it->run ();
-	delete it;
 
+	delete it;
 	delete plan;
+    
+	//----------------------------Validation
+	#pragma region validation
+	verifyDataIntegrity();
+	verifySortOrder();
+	#pragma endregion validation
 
 	return 0;
 } // main
+
+bool generateInput(int numberOfRecords, int recordSize){
+	std::fstream createdOutputFile;
+	createdOutputFile.open("output_table", std::ios::out|std::ios::binary);
+	if (!createdOutputFile.is_open()){
+        std::cout<<"Error: fail to open the file. \n";
+        return false;
+    }
+
+	std::fstream createdOutputTraceFile;
+	createdOutputTraceFile.open("trace0.txt", std::ios::out|std::ios::binary);
+	if (!createdOutputTraceFile.is_open()){
+        std::cout<<"Error: fail to open the file. \n";
+        return false;
+    }
+	
+	std::fstream generatedInput;
+	generatedInput.open("input_table", std::ios::out|std::ios::binary);
+	if (!generatedInput.is_open()){
+        std::cout<<"Error: fail to open the file. \n";
+        return false;
+    }
+
+	static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+	for (int i = 0; i < numberOfRecords; i++){
+		std::string temp;
+    	temp.reserve(recordSize);
+		for (int j = 0; j < recordSize; ++j) {
+        	temp += alphanum[rand() % (sizeof(alphanum) - 1)];
+		}
+		generatedInput << temp;
+		generatedInput <<std::endl;
+	}
+    generatedInput.close();	
+	createdOutputFile.close();
+	createdOutputTraceFile.close();
+	return true;
+}
+
---
+++ External_Merge_Sort/Verification.h
@@ -0,0 +1,2 @@
+bool verifySortOrder(); //Make sure the outputfile has records sorted
+bool verifyDataIntegrity(); //Make sure the outputfile has all inputfile records 
